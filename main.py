a8e799e293ab1c928e823b2eacf71d6946669bd7
ab-tai-404 <tailaba999@gmail.com>
2026-03-13 02:00:53 +0000

    final commit

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/m
 # Write your code below this line 👇
 
 
+response = requests.get(URL)
+web_page = BeautifulSoup(response.content, "html.parser")
+all_movies = web_page.find_all(name="h3", class_="title")
+
+with open("movies.txt", mode="w") as file:
+    for movie in all_movies[::-1]:
+        file.writelines(f"{movie.getText()}\n")
